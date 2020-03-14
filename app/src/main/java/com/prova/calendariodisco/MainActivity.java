f0f07a9fb262dedbe761aaa2c3cc09bc45ef24a2
leona <leonardodi1998@gmail.com>
2020-03-14 17:41:22 +0100

    prova

---
+++ app/src/main/java/com/prova/calendariodisco/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prova.calendariodisco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
